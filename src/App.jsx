ea80ee6026693cc413501a726e7059498337e73b
Mohamed BENRABAH <Mohamed.BENRABAH@algerietelecom.dz>
2024-12-04 17:44:11 +0100

    add suspense, error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import TaskProvider from "./contexts/TaskContext";
 import ThemeProvider from "./contexts/ThemeContext";
 import { Provider } from "react-redux";
 import store from "./stores";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
     return (
@@ -15,35 +17,39 @@ function App() {
             <ThemeProvider>
                 <TaskProvider>
                     <Router>
-                        <div className="bg-gray-100 w-full h-screen grid grid-cols-12">
-                            <div className="lg:col-span-4 xl:col-span-3 hidden lg:block">
-                                <Sidebar />
-                            </div>
-                            <div className="col-span-full lg:col-span-8 xl:col-span-9 px-4 md:px-10 xl:px-28 py-10 box-border">
-                                <Routes>
-                                    <Route
-                                        exact
-                                        path="/"
-                                        element={<HomePage />}
-                                    />
-                                    <Route
-                                        exact
-                                        path="/category"
-                                        element={<CategoryPage />}
-                                    />
-                                    <Route
-                                        exact
-                                        path="/settings"
-                                        element={<SettingsPage />}
-                                    />
-                                    <Route
-                                        exact
-                                        path="/tasks"
-                                        element={<TasksPage />}
-                                    />
-                                </Routes>
-                            </div>
-                        </div>
+                        <ErrorBoundary fallback={<h1>error</h1>}>
+                            <Suspense fallback="loading...">
+                                <div className="bg-gray-100 w-full h-screen grid grid-cols-12">
+                                    <div className="lg:col-span-4 xl:col-span-3 hidden lg:block">
+                                        <Sidebar />
+                                    </div>
+                                    <div className="col-span-full lg:col-span-8 xl:col-span-9 px-4 md:px-10 xl:px-28 py-10 box-border">
+                                        <Routes>
+                                            <Route
+                                                exact
+                                                path="/"
+                                                element={<HomePage />}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/category"
+                                                element={<CategoryPage />}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/settings"
+                                                element={<SettingsPage />}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/tasks"
+                                                element={<TasksPage />}
+                                            />
+                                        </Routes>
+                                    </div>
+                                </div>
+                            </Suspense>
+                        </ErrorBoundary>
                     </Router>
                 </TaskProvider>
             </ThemeProvider>
